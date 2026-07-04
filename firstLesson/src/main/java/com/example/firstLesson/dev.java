2cb4f613f2b198df6127167f9c354a54eb8ebb0c
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-05 00:13:24 +0545

    Autowired and constructor and setter injector

--- firstLesson/src/main/java/com/example/firstLesson/dev.java
+++ firstLesson/src/main/java/com/example/firstLesson/dev.java
@@ -1,6 +1,7 @@
 package com.example.firstLesson;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +9,26 @@ import org.springframework.stereotype.Component;
 // Now spring will create the object of the class in the container
 public class dev {
 
+    @Autowired  // this is known as field injection
+
+            // Now spring will automatically connect them and create a instance
+
+    private laptop laptop;
+
+    public void setLaptop (laptop laptop) { // setter injector
+
+
+        // We use autowired here to connect
+        this.laptop = laptop;
+    }
+
+   /* public dev(laptop lap) { // Constructor injection
+        this.lap = lap;
+    }*/
+
     public void build() {
+
+       laptop.compile();
         System.out.println("Learning Dependencies In Lesson First");
     }
 
---
+++ firstLesson/src/main/java/com/example/firstLesson/laptop.java
@@ -0,0 +1,12 @@
+package com.example.firstLesson;
+
+import org.springframework.stereotype.Component;
+
+@Component //We use component so we can invoke context like but we
+// dont want to invoke it many time soo we use autowired its help to connect
+public class laptop {
+
+    public void compile () {
+        System.out.println("Code is Compiling");
+    }
+}
