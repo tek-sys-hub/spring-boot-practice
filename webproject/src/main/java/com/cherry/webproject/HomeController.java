d37f961755aebea0f6efc8e972188f781e287698
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-07 00:16:28 +0545

    Learn how to use controller and set mapping

---
+++ webproject/src/main/java/com/cherry/webproject/HomeController.java
@@ -0,0 +1,19 @@
+package com.cherry.webproject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet () {
+        return "Welcome To Cherry Town";
+    }
+        @RequestMapping("/about")       // WE CAN SET ONE KIND OF LINKING
+    public String about () {
+        return "Am Cherry";
+    }
+
+}
--- webproject/src/main/java/com/cherry/webproject/WebprojectApplication.java
+++ webproject/src/main/java/com/cherry/webproject/WebprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebprojectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebprojectApplication.class, args);
 	}
 
