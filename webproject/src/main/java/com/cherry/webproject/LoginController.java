963de84ab098e7166fbd9aed26806874018b023a
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-08 00:18:17 +0545

    Added Login Controller

---
+++ webproject/src/main/java/com/cherry/webproject/LoginController.java
@@ -0,0 +1,14 @@
+package com.cherry.webproject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")    // WE CAN SET ONE KIND OF LINKING
+    public String about () {
+        return "Login Page";
+    }
+
+}
