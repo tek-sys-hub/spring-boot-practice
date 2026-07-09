ef7e037552eb5c1c3b4735656e9d575c9924355e
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-09 13:16:21 +0545

    Initial Commit For StreamAPI

---
+++ steamApi/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> num = Arrays.asList(4,5,6,7,8,9,10); // A list of integer
+
+    }
+}
