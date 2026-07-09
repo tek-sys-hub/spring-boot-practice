cd4b205f244c1b8f609ff85cab50469959e198de
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-09 13:35:46 +0545

    Even Finder Using Stream

--- steamApi/src/Main.java
+++ steamApi/src/Main.java
@@ -6,5 +6,13 @@ public class Main {
 
         List<Integer> num = Arrays.asList(4,5,6,7,8,9,10); // A list of integer
 
+        List<Integer> even = num.stream()
+                .filter(n -> n % 2 == 0)
+                /*We are taking n as parameter and return the value which satisfy the
+                * condition*/
+                .toList();
+
+        System.out.println(even);
+
     }
 }
