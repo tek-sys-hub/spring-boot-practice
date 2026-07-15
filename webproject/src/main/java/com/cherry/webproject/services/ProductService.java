f0c3c3ec09cec8499e8cefbf5f6b884740009760
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-15 21:57:47 +0545

    Remove All Hard Coded Values To Get From Database

--- webproject/src/main/java/com/cherry/webproject/services/ProductService.java
+++ webproject/src/main/java/com/cherry/webproject/services/ProductService.java
@@ -1,6 +1,8 @@
 package com.cherry.webproject.services;
 
 import com.cherry.webproject.models.Product;
+import com.cherry.webproject.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -12,43 +14,32 @@ import java.util.List;
 @Service
 public class ProductService {
 
+    @Autowired
+    ProductRepo repo;
+
     // Created The List Mutable
-    public List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101 , "Samsung" , 100000 ) ,
-            new Product(102 , "Camera" , 50000),
-            new Product(103 , "Laptop" , 126000)
-            ));
+//    public List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101 , "Samsung" , 100000 ) ,
+//            new Product(102 , "Camera" , 50000),
+//            new Product(103 , "Laptop" , 126000)
+//            ));
 
     public List<Product> getProducts () {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prosId) {
-        return products.stream().
-                filter(p -> p.getProsId() == prosId)
-                .findFirst().get();
+        return repo.findById(prosId).orElse(null);
     }
     public void addProduct (Product prod) {
-        products.add(prod);
+        repo.save(prod);
     }
 
     public void upprod(Product prod) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++){
-            if (products.get(i).getProsId() == prod.getProsId()) {
-                index = i;
-            }
-        }
-        products.set(index,prod);
+        repo.save(prod);
     }
 
     public void dltProd(int prodId) {
-        int index = 0;
-        for (int i = 0; i < products.size(); i++){
-            if (products.get(i).getProsId() == prodId) {
-                index = i;
-            }
-        }
-        products.remove(index);
+        repo.deleteById(prodId);
     }
 }
