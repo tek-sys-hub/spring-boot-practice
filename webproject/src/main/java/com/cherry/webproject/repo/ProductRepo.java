4a8de887ed0488cc4a364be8d987da39df2a21da
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-15 21:58:24 +0545

    Create a interface for products

---
+++ webproject/src/main/java/com/cherry/webproject/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.cherry.webproject.repo;
+
+
+import com.cherry.webproject.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+}
