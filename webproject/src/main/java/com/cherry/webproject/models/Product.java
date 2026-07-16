208f3865c0719c85d45428fdec132e8eb67b3265
tek-sys-hub <tekawasthi87@gmail.com>
2026-07-16 21:06:42 +0545

    Solve All The Constructor Problem

--- webproject/src/main/java/com/cherry/webproject/models/Product.java
+++ webproject/src/main/java/com/cherry/webproject/models/Product.java
@@ -1,12 +1,20 @@
 package com.cherry.webproject.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Component
+@Entity
 public class Product {
+    @Id
     private int prosId;
     private String proName;
     private int proPrice;
+
 }
